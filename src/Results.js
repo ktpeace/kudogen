67c0c8794f57535e24e191581fc09175515abfbd
ktpeace <katpeace@protonmail.com>
2023-01-18 16:06:42 -0500

    added some results page styling

--- src/Results.js
+++ src/Results.js
@@ -2,27 +2,69 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./results.css";
 
+// TODO
+// size words conditionally based on length
+
 const Results = ({ formEntries }) => {
   const { name, friendName, adjectives, color, description, feelings, emojis } =
     formEntries;
 
-  function adjectivesMapper() {
-    console.log(adjectives);
-    // return adjectives.map((adj) => {
-    //   return <span className="results-adj">{adj}</span>;
-    // });
-  }
+  const dummyName = "WWWWWWWWWWWWWWWWWWWW";
 
-  function feelingsMapper() {
-    // return feelings.map((feel) => {
-    //   return <span className="results-feeling">{feel}</span>;
-    // });
-  }
+  const dummyAdjectives = [
+    "playful",
+    "responsible",
+    "regal",
+    "mature",
+    "compassionate",
+    "moral",
+  ];
+
+  const dummyFeelings = [
+    "respected",
+    "confident",
+    "capable",
+    "charmed",
+    "excited",
+    "secure",
+  ];
+
+  const dummyEmojis = ["🌭", "🌿", "🎉", "👾", "👻", "🦒", "🐍", "🦑"];
+
+  // const feelingButtonGenerator = feelingList.map((word) => {
+  //   return (
+  //     <button
+  //       key={word}
+  //       value={word}
+  //       name="feeling"
+  //       type="button"
+  //       className={`feelings-button home-button ${
+  //         feelings.includes(word) ? "feelings-button-selected" : ""
+  //       }`}
+  //       onClick={handleFeelingClick}
+  //     >
+  //       {word}
+  //     </button>
+  //   );
+  // });
+  const adjectivesMapper = dummyAdjectives.map((adj) => {
+    return <span className="results-adj-feeling results-pink-text">{adj}</span>;
+  });
+
+  const feelingsMapper = dummyFeelings.map((feel) => {
+    return (
+      <span className="results-adj-feeling results-pink-text">{feel}</span>
+    );
+  });
+
+  const emojisMapper = dummyEmojis.map((emoji) => {
+    return <span className="results-emoji">{emoji}</span>;
+  });
 
   return (
     <div className="results-page">
       <main>
-        <div className="results-header">
+        <div className="results-header results-padding">
           <div>
             <h2 className="neon-major results-friendname">
               DOMINIQUE{friendName}
@@ -32,37 +74,30 @@ const Results = ({ formEntries }) => {
           <h3 className="results-kudogen">KUDOGEN</h3>
         </div>
 
-        <div className="results-four-sections">
+        <div className="results-four-sections results-padding">
           <div className="results-left">
+            {/* ADJECTIVES */}
             <section className="results-adjectives neon-minor">
               <h4>I THINK YOU ARE</h4>
-              <div>
-                <span className="results-adj">
-                  fun quirky cool hilarious compassionate
-                </span>
-                {adjectivesMapper()}
-              </div>
+              <div className="results-adj-container">{adjectivesMapper}</div>
             </section>
-            <section className="results-emojis neon-minor">
-              <h4>I ASSOCIATE YOU WITH</h4>
-              <p>
-                <span>🌭🌿🎉👾</span>
-                <span>👻🦒🐍🦑</span>
-              </p>
+            {/* EMOJI */}
+            <section className="results-emojis">
+              <h4 className=" neon-minor">I ASSOCIATE YOU WITH</h4>
+              <div className="results-emoji-container">{emojisMapper}</div>
             </section>
           </div>
 
           <div className="results-right">
+            {/* FEELINGS */}
             <section className="results-feelings neon-minor">
               <h4>YOU MAKE ME FEEL</h4>
-              <div>
-                <span className="results-feeling">happy</span>
-                {feelingsMapper()}
-              </div>
+              <div className="results-feel-container">{feelingsMapper}</div>
             </section>
+            {/* RESULT TEXT */}
             <section className="results-type neon-minor">
               <h4>MY RESULT FOR YOU IS</h4>
-              <p className="results-type-p">
+              <p className="results-type-p results-pink-text">
                 squid! The squid has spent many years traversing the depths,
                 acquiring quirky charm. what has it seen? what will it do next?
                 we can only wait and wonder...
@@ -73,7 +108,7 @@ const Results = ({ formEntries }) => {
 
         <section className="results-image-footer">
           <p>
-            Made for you by {name} at KUDOGEN. What animal represents your
+            Made for you by {dummyName} at KUDOGEN. What animal represents your
             friends? Generate a compliment at http://www.sitename.com
           </p>
         </section>
